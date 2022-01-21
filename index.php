fdabd012c4accf41e5a8723d50435ef3d0448057
Christoph Burgdorfer <christoph.burgdorfer@gmail.com>
2022-01-21 10:53:52 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+include_once("credentials.php"); // use credentials.php.sample
+
+// check if the post is legit by comparing checksums
+if($_POST['checksum'] != md5(SALT.$_POST['timestamp'])) {
+	die("Checksum mismatch");
+}
+
+// Create connection
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASS);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+echo "connection successful.";

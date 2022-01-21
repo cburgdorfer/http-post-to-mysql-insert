b0551dd1c914877b30ad65e4f6af6e2579f872d2
Christoph Burgdorfer <christoph.burgdorfer@gmail.com>
2022-01-21 11:16:54 +0800

    disable checksum for now

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 include_once("credentials.php"); // use credentials.php.sample
 
 // check if the post is legit by comparing checksums
-if($_POST['checksum'] != md5(SALT.$_POST['timestamp'])) {
-	die("Checksum mismatch");
-}
+// if($_POST['checksum'] != md5(SALT.$_POST['timestamp'])) {
+//	die("Checksum mismatch");
+// }
 
 // Create connection
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASS);

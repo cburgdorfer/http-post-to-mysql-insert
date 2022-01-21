3acd4cb2c56ca2d7b1d58d0c1c03797c3b96a7d9
Christoph Burgdorfer <christoph.burgdorfer@gmail.com>
2022-01-21 20:50:10 +0800

    various implementations

--- index.php
+++ index.php
@@ -8,11 +8,52 @@ include_once("credentials.php"); // use credentials.php.sample
 // }
 
 // Create connection
-$conn = new mysqli(DB_HOST, DB_USER, DB_PASS);
+$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS);
 
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+// Get the JSON contents
+$json = file_get_contents('php://input');
+
+// decode the json data
+$data = json_decode($json);
+
+// Check if request is legit
+if(md5($data["Area"].
+	$data["DataID"].
+	$data["DataKind"].
+	$data["DateTime"].
+	$data["Layer"].
+	$data["LogicalDevice"].
+	$data["SenderID"].
+	$data["Value"].
+	SALT) != $data["Checksum"]) {
+		die("Checksum failed.");
 }
 
-echo "connection successful.";
+// Check connection
+if ($mysqli->connect_error) {
+  die("Connection failed: " . $mysqli->connect_error);
+}
+
+$sql = "INSERT INTO gravio_data 
+	(Area, Layer, DataKind, LogicalDevice, SenderID, DataID, DateTime, Value, created)
+VALUES 
+	(?, ?, ?, ?, ?, ?, ?, ?, NOW())";
+	
+$stmt = $mysqli->prepare($sql);
+$stmt->bind_param("ssssssss", 	$data["Area"],
+								$data["DataID"],
+								$data["DataKind"],
+								$data["DateTime"],
+								$data["Layer"],
+								$data["LogicalDevice"],
+								$data["SenderID"],
+								$data["Value"]);
+
+
+if ($stmt->execute(); === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $mysqli->error;
+}
+
+$mysqli->close();
